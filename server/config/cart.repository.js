3bcefa4bd68958f87cbd9bd54e183e28292da085
RBunna <bunnasovethroath@gmail.com>
2025-07-23 13:03:01 +0700

    Delete messed files, and initialize new controller

---
+++ server/config/cart.repository.js
@@ -0,0 +1,26 @@
+import { cartItems } from "../module/CartItemDb.js";
+import { Users } from "../module/usersDb.js";
+import { Books } from "../module/BookDb.js";
+
+
+async function getCart(u_id) {
+  const usr = await Users.findByPk(u_id, { include: [{ model: cartItems, include: [{ model: Books }] }] });
+  if (!usr) {
+    throw new Error("User not found");
+  }
+
+  return usr.toJSON();
+}
+
+async function addToCart(u_id, b_id, qty) {
+
+}
+try {
+  await getCart(1);
+  
+} catch (error) {
+  
+}
+console.log("Hello");
+
+export { getCart, addToCart };
